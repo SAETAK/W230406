001c3315c274cc8b909a4667446a400cb5f875f6
SAETAK <ihjang404@gmail.com>
2023-04-06 09:36:04 +0900

    First Save

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+//Slime. Goblin, WildBoar
+
+class Player
+{
+	int Hp = 0;
+	int Mp = 0;
+	int PlayerX = 1;
+	int PlayerY = 1;
+};
+
+class Monster
+{
+	int MonsterX = 1;
+	int MonsterY = 1;
+};
+
+int main()
+{
+	
+	return 0;
+}
