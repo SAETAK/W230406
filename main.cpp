7ecf937074b58c9382a5fecd1deb45d40fff2b59
SAETAK <ihjang404@gmail.com>
2023-04-06 11:18:35 +0900

    New Save

--- main.cpp
+++ main.cpp
@@ -1,26 +1,8 @@
 #include <iostream>
-#include <conio.h>
 
 using namespace std;
 
-//Slime. Goblin, WildBoar
-
-class Player
-{
-	int Hp = 0;
-	int Mp = 0;
-	int PlayerX = 1;
-	int PlayerY = 1;
-};
-
-class Monster
-{
-	int MonsterX = 1;
-	int MonsterY = 1;
-};
-
 int main()
 {
-	
 	return 0;
 }
