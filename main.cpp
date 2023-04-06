421656e3db68b561ce6d39fd374ecbbfe49cee39
SAETAK <ihjang404@gmail.com>
2023-04-06 12:52:00 +0900

    Add Slime, Goblin, WildBoar 생성

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include "FPlayer.h"
+#include "FSlime.h"
+#include "FGoblin.h"
+#include "FWildBoar.h"
 
 using namespace std;
 
 int main()
 {
 	FPlayer* Player = new FPlayer();
+	FSlime* Slime = new FSlime();
+	FGoblin* Goblin = new FGoblin();
+	FWildBoar* WildBoar = new FWildBoar();
 
-	cout << Player << endl;
 
 	delete Player;
 	return 0;
